fd7454d57d7cf3063094de6bd1039d53e972cadd
allenstu6311 <stuallen6311@gmail.com>
2024-02-10 17:45:26 +0800

    ig

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -12,6 +12,13 @@ export class HomeComponent {
   @ViewChild('text', { static: true }) text!: ElementRef;
   @ViewChild('openAnimation', { static: true }) openAnimation!: ElementRef;
   display: boolean = false;
+  igPic:string[] = [
+    'https://desertturtle.art/wp-content/uploads/sb-instagram-feed-images/414501706_1089274919184158_6860997398438781024_nlow.jpg',
+
+    'https://desertturtle.art/wp-content/uploads/sb-instagram-feed-images/412538420_1093944088270542_8936042847412642815_nlow.jpg',
+    'https://desertturtle.art/wp-content/uploads/sb-instagram-feed-images/414501706_1089274919184158_6860997398438781024_nlow.jpg', 'https://desertturtle.art/wp-content/uploads/sb-instagram-feed-images/414501706_1089274919184158_6860997398438781024_nlow.jpg',
+    'https://desertturtle.art/wp-content/uploads/sb-instagram-feed-images/414501706_1089274919184158_6860997398438781024_nlow.jpg'
+  ]
 
   constructor(){
     timer(2500).subscribe(() => {
