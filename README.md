6c92941f56ee0cc4b09b13998b7af879f392b494
allenstu6311 <stuallen6311@gmail.com>
2024-02-13 18:16:19 +0800

    update

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## 部屬問題
+這次被路徑問題雷得很慘，引入圖片不需要../等對路徑設定，baseHref即可，在angular.json以及index.html
--- src/app/view/concat/concat.component.ts
+++ src/app/view/concat/concat.component.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
@@ -8,37 +18,64 @@ import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
   styleUrls: ['./concat.component.scss'],
 })
 export class ConcatComponent {
+  @ViewChild('lightBox', { static: false }) lightBox!: ElementRef;
+  @ViewChild('lightboxDesc', { static: false }) lightboxDesc!: ElementRef;
   mailForm: FormGroup | any;
-  constructor(private fb: FormBuilder) {}
+  sendStatus: boolean = false;
+  showBox: boolean = false;
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.mailForm = this.fb.group({
-      name: [''],
-      email: [''],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       lineId: [''],
       message: [''],
     });
   }
 
   sendEmail() {
-    const templateParams = {
-      name: this.mailForm.value.name,
-      email: this.mailForm.value.email,
-      lineId: this.mailForm.value.lineId,
-      message: this.mailForm.value.message
-    };
+    if (this.mailForm.invalid) {
+    } else {
+      const templateParams = {
+        name: this.mailForm.value.name,
+        email: this.mailForm.value.email,
+        lineId: this.mailForm.value.lineId,
+        message: this.mailForm.value.message,
+      };
+      this.showBox = true;
+      setTimeout(() => {
+        console.log(this.lightBox);
+        this.lightBox.nativeElement.focus();
+      }, 1000);
+   
 
-    emailjs
-      .send('service_9y4pk49', 'template_rn4wtkf', templateParams , {
-        publicKey: 'c_mRBMNUzJXD7c1m4',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', (error as EmailJSResponseStatus).text);
-        }
-      );
+      // emailjs
+      //   .send('service_9y4pk49', 'template_rn4wtkf', templateParams , {
+      //     publicKey: 'c_mRBMNUzJXD7c1m4',
+      //   })
+      //   .then(
+      //     () => {
+      //       console.log('SUCCESS!');
+      //       this.sendStatus = true
+      //       this.showBox = true
+      //         setTimeout(() => {
+      //         // Wait for the next cycle so the DOM is updated
+      //         this.lightBox.nativeElement.focus();
+      //       }, 0);
+      //     },
+      //     (error) => {
+      //       this.sendStatus = false
+      //       this.showBox = true
+      //       // this.lightBox.nativeElement.focus()
+      //       setTimeout(() => {
+      //         // Wait for the next cycle so the DOM is updated
+      //         this.lightBox.nativeElement.focus();
+      //       }, 0);
+      //       console.log('FAILED...', (error as EmailJSResponseStatus).text);
+      //     }
+      //   );
+    }
   }
 }
